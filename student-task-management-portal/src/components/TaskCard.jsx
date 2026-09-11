8e552cb8853f60ed7df1377e6c07187a217902ac
ashankdavid <ashank2507david@gmail.com>
2026-09-11 16:18:56 +0530

    creation of taskDetails.jsx and starting to pass the data, uplifted the data from dashboard to app.jsx

--- student-task-management-portal/src/components/TaskCard.jsx
+++ student-task-management-portal/src/components/TaskCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function TaskCard(props) {
     return (
         <div className="task-card">
@@ -12,6 +14,9 @@ function TaskCard(props) {
             <button onClick={props.onDelete}>
                 Delete
             </button>
+            <Link to={`/tasks/${props.id}`}>
+                View Details
+            </Link>
         </div>
     );
 }
